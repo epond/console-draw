a47968bdb54a1b6bf2a8288acb805d8303b2a1e4
epond <epond@users.noreply.github.com>
2015-02-13 23:47:54 +0000

    better Canvas unit test

--- src/test/scala/drawing/CanvasSpec.scala
+++ src/test/scala/drawing/CanvasSpec.scala
@@ -3,36 +3,44 @@ package drawing
 import org.specs2.mutable.Specification
 
 class CanvasSpec extends Specification {
-  val exampleCanvas = """----------------------
-                        ||                    |
-                        ||xxxxxx              |
-                        ||     x              |
-                        ||     x              |
-                        |----------------------""".stripMargin
+  val emptyCanvas = """----------------------
+                      ||                    |
+                      ||                    |
+                      ||                    |
+                      ||                    |
+                      |----------------------""".stripMargin
+  val canvasWithTwoLines = """----------------------
+                             ||                    |
+                             ||xxxxxx              |
+                             ||     x              |
+                             ||     x              |
+                             |----------------------""".stripMargin
   "A Canvas" should {
+    "be creatable from width and height values" in {
+      Canvas(20, 4).toString must beEqualTo(emptyCanvas)
+    }
     "implement parsing and toString as the inverse of each other" in {
-      val parsedCanvas = Canvas(exampleCanvas)
-      parsedCanvas.toString must beEqualTo(exampleCanvas)
+      Canvas(canvasWithTwoLines).toString must beEqualTo(canvasWithTwoLines)
     }
 
     "implement height" in {
-      Canvas(exampleCanvas).height must beEqualTo(4)
+      Canvas(canvasWithTwoLines).height must beEqualTo(4)
     }
 
     "implement width" in {
-      Canvas(exampleCanvas).width must beEqualTo(20)
+      Canvas(canvasWithTwoLines).width must beEqualTo(20)
     }
 
     "when getNode is called on an empty space then return the empty character" in {
-      Canvas(exampleCanvas).getNode(Coordinates(1, 3)) must beEqualTo(Some(emptyPosition))
+      Canvas(canvasWithTwoLines).getNode(Coordinates(1, 3)) must beEqualTo(Some(emptyPosition))
     }
 
     "when getNode is called on a line then return the line character" in {
-      Canvas(exampleCanvas).getNode(Coordinates(6, 3)) must beEqualTo(Some('x'))
+      Canvas(canvasWithTwoLines).getNode(Coordinates(6, 3)) must beEqualTo(Some('x'))
     }
 
     "when getNode is called out of bounds then return None" in {
-      Canvas(exampleCanvas).getNode(Coordinates(1, 5)) must beNone
+      Canvas(canvasWithTwoLines).getNode(Coordinates(1, 5)) must beNone
     }
   }
 }
